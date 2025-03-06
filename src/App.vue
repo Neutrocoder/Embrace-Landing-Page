1b293b9327ea976e94070ccac97fd5687964ae4f
Neutrocoder <luismedinadeveloper@gmail.com>
2025-03-06 12:36:23 -0600

    Initial commit: Vue + Tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import Partners from './components/Partners.vue';
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <Partners />
+</template>
+
+<style scoped>
+
+</style>
+{
